7c273e515bda107e01bab9129536fdffb948e90d
LuisMunguiaDevelop <develop.luismunguia@gmail.com>
2024-09-25 16:23:27 -0700

    :adhesive_bandage: fixed character list implementing new grid strategy

--- composeApp/src/commonMain/kotlin/org/plux/marvelpedia/features/characters/character_search/CharacterSearchScreen.kt
+++ composeApp/src/commonMain/kotlin/org/plux/marvelpedia/features/characters/character_search/CharacterSearchScreen.kt
@@ -1,17 +1,24 @@
 package org.plux.marvelpedia.features.characters.character_search
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import org.koin.compose.viewmodel.koinViewModel
 import org.plux.marvelpedia.commons.ui.SearchBarComponent
-import org.plux.marvelpedia.features.characters.character_list.CharacterLazyList
+import org.plux.marvelpedia.features.characters.character_detail.CharacterDetailScreen
+import org.plux.marvelpedia.features.characters.character_list.ui.CharacterItem
 import org.plux.marvelpedia.theme.primaryColor
 
 class CharacterSearchScreen : Screen {
@@ -23,7 +30,6 @@ class CharacterSearchScreen : Screen {
 
         CharacterSearchContent(
             uiState = uiState,
-            //fetchList = { },
             onCharacterSearch = { viewModel.getList(it) },
             onBackPressed = { viewModel.onBackPressed() }
         )
@@ -34,7 +40,6 @@ class CharacterSearchScreen : Screen {
 @Composable
 fun CharacterSearchContent(
     uiState: CharacterSearchState,
-    //fetchList: () -> Unit = {},
     onCharacterSearch: (String) -> Unit = {},
     onBackPressed: () -> Unit = {}
 ) {
@@ -63,12 +68,24 @@ fun CharacterSearchContent(
     ) {
 
         //Scaffold content
-        CharacterLazyList(
-            characterList = uiState.list,
-            isFetchable = false
-        )
 
-        //End of Scaffold content
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(4),
+            contentPadding = PaddingValues(10.dp),
+            modifier = Modifier
+                .background(color = primaryColor)
+                .fillMaxSize(),
+
+            ) {
+            items(uiState.list) { character ->
+                CharacterItem(
+                    character = character,
+                    onClick = { navigator.push(CharacterDetailScreen(character = it)) }
+                )
+            }
+
+            //End of Scaffold content
+        }
     }
 }
 
