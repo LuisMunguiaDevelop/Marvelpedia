cac10b9bf9322ed9a5284dcdee72a8cebbf7836f
LuisMunguiaDevelop <develop.luismunguia@gmail.com>
2024-09-26 13:58:34 -0700

    :recycle: implemented reusable state in search character

--- composeApp/src/commonMain/kotlin/org/plux/marvelpedia/features/characters/character_search/CharacterSearchViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/plux/marvelpedia/features/characters/character_search/CharacterSearchViewModel.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import org.plux.marvelpedia.commons.model.SearchScreenState
 import org.plux.marvelpedia.commons.model.handleFilteredList
 import org.plux.marvelpedia.features.characters.character_list.data.use_cases.get_character_list.GetCharacterListUC
 import org.plux.marvelpedia.features.characters.character_list.model.Character
@@ -18,7 +19,7 @@ import org.plux.marvelpedia.network.ApiResponse
 class CharacterSearchViewModel(
     private val getCharacterListUC: GetCharacterListUC
 ) : ViewModel() {
-    var state by mutableStateOf(CharacterSearchState())
+    var state by mutableStateOf(SearchScreenState<Character>())
         private set
 
     fun getList(nameFilter: String = "") {
@@ -31,7 +32,7 @@ class CharacterSearchViewModel(
     }
 
     fun onBackPressed(){
-        state = CharacterSearchState()
+        state = SearchScreenState()
     }
 
     private fun getFilteredList(nameFilter: String = "") = viewModelScope.launch(Dispatchers.IO) {
