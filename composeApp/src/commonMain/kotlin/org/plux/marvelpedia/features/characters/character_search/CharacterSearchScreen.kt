c98708efdd0c447614121eeaf31617841886866d
LuisMunguiaDevelop <develop.luismunguia@gmail.com>
2024-09-26 10:32:59 -0700

    :recycle: change CharacterSearchScreen structure

--- composeApp/src/commonMain/kotlin/org/plux/marvelpedia/features/characters/character_search/CharacterSearchScreen.kt
+++ composeApp/src/commonMain/kotlin/org/plux/marvelpedia/features/characters/character_search/CharacterSearchScreen.kt
@@ -1,25 +1,14 @@
 package org.plux.marvelpedia.features.characters.character_search
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import org.koin.compose.viewmodel.koinViewModel
-import org.plux.marvelpedia.commons.ui.SearchBarComponent
+import org.plux.marvelpedia.commons.ui.CollectionSearchScreen
 import org.plux.marvelpedia.features.characters.character_detail.CharacterDetailScreen
 import org.plux.marvelpedia.features.characters.character_list.ui.CharacterItem
-import org.plux.marvelpedia.theme.primaryColor
 
 class CharacterSearchScreen : Screen {
 
@@ -27,65 +16,26 @@ class CharacterSearchScreen : Screen {
     override fun Content() {
         val viewModel = koinViewModel<CharacterSearchViewModel>()
         val uiState by lazy { viewModel.state }
+        val navigator = LocalNavigator.currentOrThrow
 
-        CharacterSearchContent(
-            uiState = uiState,
-            onCharacterSearch = { viewModel.getList(it) },
-            onBackPressed = { viewModel.onBackPressed() }
+        CollectionSearchScreen(
+            isLoading = uiState.isLoading,
+            initialText = uiState.nameFilter,
+            placerHolderString = "Search a Character",
+            onTypedDetected = { viewModel.getList(it) },
+            onBackPressed = {
+                navigator.pop()
+                viewModel.onBackPressed()
+            },
+            gridContent = {
+                items(uiState.list) { character ->
+                    CharacterItem(
+                        character = character,
+                        onClick = { navigator.push(CharacterDetailScreen(character)) }
+                    )
+                }
+            }
         )
-    }
-
-}
-
-@Composable
-fun CharacterSearchContent(
-    uiState: CharacterSearchState,
-    onCharacterSearch: (String) -> Unit = {},
-    onBackPressed: () -> Unit = {}
-) {
-    val navigator = LocalNavigator.currentOrThrow
-    val showSearchBar = remember { mutableStateOf(true) }
-
-    Scaffold(
-        backgroundColor = primaryColor,
-        topBar = {
-            if (showSearchBar.value) {
-                SearchBarComponent(
-                    placeHolderString = "Search a character",
-                    onTyped = {
-                        onCharacterSearch.invoke(it)
-                    },
-                    isLoading = uiState.isLoading,
-                    onBackPressed = {
-                        navigator.pop()
-                        onBackPressed.invoke()
-                    },
-                    initialText = uiState.nameFilter
-                )
-            }
-        },
-        modifier = Modifier.fillMaxSize()
-    ) {
-
-        //Scaffold content
-
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(4),
-            contentPadding = PaddingValues(10.dp),
-            modifier = Modifier
-                .background(color = primaryColor)
-                .fillMaxSize(),
-
-            ) {
-            items(uiState.list) { character ->
-                CharacterItem(
-                    character = character,
-                    onClick = { navigator.push(CharacterDetailScreen(character = it)) }
-                )
-            }
-
-            //End of Scaffold content
-        }
     }
 }
 
